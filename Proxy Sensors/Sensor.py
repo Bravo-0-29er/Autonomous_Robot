74b25a7b35f7cd74ea6ba1cab4be8633a7524245
blinger <robert.franklin.linger@gmail.com>
2022-03-30 12:54:21 -0400

    Moved pulse code inside while loop

--- Proxy Sensors/Sensor.py
+++ Proxy Sensors/Sensor.py
@@ -1,35 +1,39 @@
 import RPi.GPIO as GPIO
 import time
 
+# https://thepihut.com/blogs/raspberry-pi-tutorials/hc-sr04-ultrasonic-range-sensor-on-the-raspberry-pi
+
 # define constants
 TRIG = 23 # pin that triggers the sensor
 ECHO = 24 # pin that reads signal return from the sensor
 
-print("Distance Measured In Progress")
+# print("Distance Measured In Progress")
 
 # configure GPIO pins
 GPIO.setmode(GPIO.BCM) # configure pin numbering
 GPIO.setup(TRIG,GPIO.OUT)
 GPIO.setup(ECHO,GPIO.IN)
-
-# create trigger pulse
-GPIO.output(TRIG, False)
-time.sleep(2) # wait for sensor to settle 
-GPIO.output(TRIG, True)
-time.sleep(0.00001)
 GPIO.output(TRIG, False)
 
-# get time stamps for start and end of pulse
-while GPIO.input(ECHO)==0:
-    pulse_start = time.time()
-while GPIO.input(ECHO)==1:
-    pulse_end = time.time()
+while True:
 
-# calculate distance based on pulse duration
-pulse_duration = pulse_end - pulse_start
-distance = pulse_duration * 17150
-distance = round(distance, 2)
+    time.sleep(1)
+    
+    # create sensor pulse
+    GPIO.output(TRIG, True)
+    time.sleep(0.00001)
+    GPIO.output(TRIG, False)
 
-print("Distance: " + distance + " cm")
+    # get time stamps for start and end of pulse
+    while GPIO.input(ECHO)==0:
+        pulse_start = time.time()
+    while GPIO.input(ECHO)==1:
+        pulse_end = time.time()
+
+    # calculate distance based on pulse duration
+    pulse_duration = pulse_end - pulse_start
+    distance = pulse_duration * 17150
+    distance = round(distance, 2)
+    print("Distance: " + distance + " cm")
 
 GPIO.cleanup() 

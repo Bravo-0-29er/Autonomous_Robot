4dc6610b83fd9a959e8d8310aa62a94edeec4aeb
Bravo-0-29er <78572225+Bravo-0-29er@users.noreply.github.com>
2022-04-27 00:57:29 -0400

    Update/Backup of files
    Main.py and Sensor.py updated to operate both wheels and stop when in proximity to an object.
    Backup of MotorControler.py and PCA9685.py due to failing SD card

--- Main.py
+++ Main.py
@@ -2,7 +2,7 @@
 
 from multiprocessing import Process,Queue,Pipe
 import time
-from Sensor import getDistance
+from Sensor import getDistance_right,getDistance_left
 from PCA9685 import PCA9685
 import time
 
@@ -53,41 +53,47 @@ class MotorDriver():
             pwm.setDutycycle(self.PWMB, 0)
             
 def halt():
-    Motor.MotorRun(0, 'forward', 50)
-    Motor.MotorRun(1, 'forward', 50)
-    time.sleep(0.5)
-    Motor.MotorRun(0, 'forward', 25)
-    Motor.MotorRun(1, 'forward', 25)
-    time.sleep(0.5)
+    Motor.MotorRun(0, 'forward', 75)
+    Motor.MotorRun(1, 'forward', 75)
+    time.sleep(0.25)
+    Motor.MotorRun(0, 'forward', 30)
+    Motor.MotorRun(1, 'forward', 30)
+    time.sleep(0.25)
     Motor.MotorStop(0)
     Motor.MotorStop(1)
     
 def move():
-    Motor.MotorRun(0, 'forward', 75)
-    Motor.MotorRun(1, 'forward', 75)
-            
-Motor = MotorDriver()
+    Motor.MotorRun(0, 'forward', 100)
+    Motor.MotorRun(1, 'forward', 100)
 
-print("Main File")
-print("Calling Sensor.py")
-if __name__ == '__main__':
-    parent_conn,child_conn = Pipe()
-    p = Process(target=getDistance, args=(child_conn,))
-    p.start()
-    Running = True;
-    while True:
-        distance = parent_conn.recv()
-        if distance < 50.0:
-            print("Stop the cart")
-            if Running:
-                halt()
-                Running = False
-        else:
-            print("Continue")
-            if not Running:
-                move()
-                Running = True
-        time.sleep(1)
+print(" press Ctrl+c to stop program ")
+try:            
+    Motor = MotorDriver()
 
-Motor.MotorStop(0)
-Motor.MotorStop(1)
+    #print("Main File")
+    #print("Calling Sensor.py")
+    if __name__ == '__main__':
+        parent_conn_left,child_conn_left = Pipe()
+        p_left = Process(target=getDistance_left, args=(child_conn_left,))
+        p_left.start()
+        parent_conn_right,child_conn_right = Pipe()
+        p_right = Process(target=getDistance_right, args=(child_conn_right,))
+        p_right.start()
+        Running = False;
+        while True:
+            distance_left = parent_conn_left.recv()
+            distance_right = parent_conn_right.recv()
+            if distance_left < 25.0 or distance_right < 25.0:
+                print("Stop the cart")
+                if Running:
+                    halt()
+                    Running = False
+            else:
+                print("Continue")
+                if not Running:
+                    move()
+                    Running = True
+            time.sleep(1)
+except KeyboardInterrupt:
+    Motor.MotorStop(0)
+    Motor.MotorStop(1)
